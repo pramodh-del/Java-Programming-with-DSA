45b250db05bd8a90e7a9157b54bc0c0e4336dd76
KADAM PRAMODH <70189543+pramodh-del@users.noreply.github.com>
2023-12-29 21:44:14 +0530

    day 10

---
+++ MaxElement.java
@@ -0,0 +1,14 @@
+public class MaxElement {
+    public static void main(String[] args) {
+        int[] numbers = {5, 8, 2, 10, 3};
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum element in the array: " + max);
+    }
+}
