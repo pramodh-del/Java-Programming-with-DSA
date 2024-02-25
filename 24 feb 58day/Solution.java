c1acc0763ac569805028beb6b9e4b65761ea326e
KADAM PRAMODH <70189543+pramodh-del@users.noreply.github.com>
2024-02-25 18:38:57 +0530

    majority element

--- 24 feb 58day/Solution.java
+++ 24 feb 58day/Solution.java
@@ -1,14 +1,30 @@
+import java.util.StringTokenizer;
+
 class Solution {
     public static void main(String[] args) {
 
     }
     static String reverseWords(String s) {
+        StringBuilder sc=new StringBuilder();
+        for (int i = 0; i <s.length(); i++) {
+            char ch=s.charAt(i);
+            sc.append(ch);
+
+        }
+        sc.reverse();
+        String a=sc.toString();
         int i=0;
-        int l=0;
         int r=0;
-        int n=s.length();
+        int l=0;
+        int n=a.length();
+        String result="";
         while (i<n){
-            while ()
+            char ch=s.charAt(i);
+            char ch1=result.charAt(i);
+            while (i<n&&ch!=' '){
+
+
+            }
         }
         
     }
